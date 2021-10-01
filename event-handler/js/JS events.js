b8d1c887f72d5315ca87307ce33758c35694a24d
Benson Tan <benson.hmtan@gmail.com>
2021-10-01 20:37:49 +0800

    push

--- event-handler/js/JS events.js
+++ event-handler/js/JS events.js
@@ -6,3 +6,17 @@ let e1NamaPengguna = document.getElementById("namaPengguna")
 e1NamaPengguna.addEventListener("keyup",function(){
     e1NamaPengguna.value = tranformUppercase(e1NamaPengguna.value)
 })
+
+function checkPassword(){
+    let e1KataLaluan = document.getElementById("kataLaluan")
+    let pass_val = e1KataLaluan.value
+
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+    } else{
+        alert("Password too short!")
+    }
+}
+
+let e1KL = document.getElementById("kataLaluan")
+e1KL.onblur = checkPassword
