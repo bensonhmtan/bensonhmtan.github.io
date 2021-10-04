d9a818ab0c834802a9f2fb6892d95c28ce20fe66
Benson Tan <benson.hmtan@gmail.com>
2021-10-04 20:13:09 +0800

    push

--- geolocation/js/JS haversine_main.js
+++ geolocation/js/JS haversine_main.js
@@ -9,7 +9,25 @@ function calculateDistances(lat,lon){
 let e1Locate = document.getElementById("locate")
 e1Locate.addEventListener("click", function(){
     if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            let e1Lat = document.getElementById("lat")
+            let e1Long = document.getElementById("long")
+            let e1Dpulze = document.getElementById("depulze")
+            let e1Klcc = document.getElementById("klcc")
+            let e1Sunway = document.getElementById("sunway")
 
+            let userLat = position.coords.latitude
+            let userLong = position.coords.longitude
+
+            let distances = calculateDistances(userLat, userLong)
+
+            e1Lat.innerHTML = "Your latitude: "+ userLat
+            e1Long.innerHTML = "Your longitude: "+ userLong
+            e1Dpulze.innerHTML = "Distance to Dpulze, Cyberjaya is " + distances [0] + "km"
+            e1Klcc.innerHTML = "Distance to KLCC is " + distances [2] + "km"
+            e1Sunway.innerHTML = "Distance to Sunway Pyramid is " + distances [1] + "km"
+
+        })
     } else {
         alert("Geolocation is not supported!")
     }
