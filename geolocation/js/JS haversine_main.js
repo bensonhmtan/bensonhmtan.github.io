bb8268588b463e513635798f69739810d43943e9
Benson Tan <benson.hmtan@gmail.com>
2021-10-04 20:45:55 +0800

    push

--- geolocation/js/JS haversine_main.js
+++ geolocation/js/JS haversine_main.js
@@ -1,32 +1,31 @@
-function calculateDistances(lat,lon){
+function calculateDistances(lat, lon){
     let dpulze = haversine(lat,lon,2.922561,101.650965)
-    let sunway = haversine(lat,lon,3.073065,101.607787)
+    let pyramid = haversine(lat,lon,3.073065,101.607787)
     let klcc = haversine(lat,lon,3.158761,101.714524)
 
-    return [dpulze,sunway,klcc]
+    return [dpulze,pyramid,klcc]
 }
 
-let e1Locate = document.getElementById("locate")
-e1Locate.addEventListener("click", function(){
+let elLocate = document.getElementById("locate")
+elLocate.addEventListener("click", function(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(function(position){
-            let e1Lat = document.getElementById("lat")
-            let e1Long = document.getElementById("long")
-            let e1Dpulze = document.getElementById("depulze")
-            let e1Klcc = document.getElementById("klcc")
-            let e1Sunway = document.getElementById("sunway")
+            let elLat = document.getElementById("lat")
+            let elLong = document.getElementById("long")
+            let elDpulze =document.getElementById("depulze")
+            let elKlcc = document.getElementById("klcc")
+            let elSunway = document.getElementById("sunway")
 
             let userLat = position.coords.latitude
             let userLong = position.coords.longitude
 
             let distances = calculateDistances(userLat, userLong)
 
-            e1Lat.innerHTML = "Your latitude: "+ userLat
-            e1Long.innerHTML = "Your longitude: "+ userLong
-            e1Dpulze.innerHTML = "Distance to Dpulze, Cyberjaya is " + distances [0] + "km"
-            e1Klcc.innerHTML = "Distance to KLCC is " + distances [2] + "km"
-            e1Sunway.innerHTML = "Distance to Sunway Pyramid is " + distances [1] + "km"
-
+            elLat.innerHTML = "Your latitude: " + userLat
+            elLong.innerHTML = "Your longitude: " + userLong
+            elDpulze.innerHTML = "Distance to Dpluze, Cyberjaya is " + distances[0] + "km"
+            elKlcc.innerHTML = "Distance to KLCC is " + distances[2] + "km"
+            elSunway.innerHTML = "Distance to Sunway Pyramid is " + distances[1] + "km"
         })
     } else {
         alert("Geolocation is not supported!")
