e42035aeecf2c285138202fdd1b73c73b204a7cc
Benson Tan <benson.hmtan@gmail.com>
2021-10-04 20:23:28 +0800

    push

--- geolocation/js/JS haversine_main.js
+++ geolocation/js/JS haversine_main.js
@@ -1,9 +1,9 @@
 function calculateDistances(lat,lon){
     let dpulze = haversine(lat,lon,2.922561,101.650965)
-    let pyramid = haversine(lat,lon,3.073065,101.607787)
+    let sunway = haversine(lat,lon,3.073065,101.607787)
     let klcc = haversine(lat,lon,3.158761,101.714524)
 
-    return [dpulze,pyramid,klcc]
+    return [dpulze,sunway,klcc]
 }
 
 let e1Locate = document.getElementById("locate")
