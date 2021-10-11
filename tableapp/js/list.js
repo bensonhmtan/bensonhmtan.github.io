ef3ffaf1d2406a0908f835e5159bfd9767cc19cf
Benson Tan <benson.hmtan@gmail.com>
2021-10-11 20:44:20 +0800

    push

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -14,7 +14,7 @@ function GetBooking(){
             let bookingNameList = document.getElementById("bookingNameList")
 
             //delete all rows in the table
-            for(let k = bookingNameList.rows.length - 1; k >= 0; k --){
+            for(let k = bookingNameList.rows.length - 1; k >= 0; k--){
                 bookingNameList.deleteRow(k)
             }
 
